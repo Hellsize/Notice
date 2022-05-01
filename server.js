d22864577d4443b9da52e1fa6426b7cff19d27c8
Hellsize <dmitrii-walk@mail.ru>
2022-05-01 22:07:10 +0700

    01.05

--- server.js
+++ server.js
@@ -2,32 +2,36 @@ const express = require('express'),
     app = express(),
     session = require('express-session')
 
-const host = localhost;
-const port = 3000;
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static('templates/images'));
-
-
+app.use(express.static(__dirname + '/views/static'));
+const nunjucks = require('nunjucks');
+nunjucks.configure(__dirname + '/views', {
+    autoescape: true,
+    express: app
+});
 
 app.use(
     session({
         secret: 'you secret key',
         saveUninitialized: true,
+        resave:true,
     })
 )
 
 app.get('/', function(req, res) {
-    res.render('index.html', { name: 'leo' });
+    res.render(__dirname + '/views/index.html', );
 });
 
-app.get('/', (req, res) => {
-    response.send(__dirname + "/templates/index.html")
-    console.log(req.session.showAd)
+app.get('/message', function(req, res) {
+    res.render(__dirname + "/views/message.njk")
+
 
 })
-
+app.get('/reg',function(req,res){
+    res.render(__dirname + "/views/reg.html")
+})
 
 app.listen(3000, function() {
-    console.log(`Server listens http://${host}:${port}`)
+    console.log(`Servers`)
 })
