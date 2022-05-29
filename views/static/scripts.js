27c914477cc3df37893b2ba8f1f6d4e4c532177c
Hellsize <dmitrii-walk@mail.ru>
2022-05-29 15:30:33 +0700

    29.05

--- views/static/scripts.js
+++ views/static/scripts.js
@@ -1,17 +1,17 @@
 function addTask() {
     let newdiv = document.createElement("div");
-    newdiv.innerHTML = "<input type='text' class='txt'>" +  "<img src='/images/check.png' class='checked'>"
+    newdiv.innerHTML = "<input type='text' class='txt' placeholder='Description' >" + "<img src='/images/check.png' class='checked'>" + "<input type='text' class='date' placeholder='deadline'"
     newdiv.className = "NewTask"
     newdiv.id = "draggable-1"
     newdiv.setAttribute("draggable", "true");
-    newdiv.setAttribute("ondragstart","onDragStart(event);")
-    newdiv.setAttribute("dragend","DragEnd(event);")
+    newdiv.setAttribute("ondragstart", "onDragStart(event);")
+    newdiv.setAttribute("dragend", "DragEnd(event);")
     console.log("okay")
     document.getElementById("addTask").appendChild(newdiv)
 
- let task = document.getElementById("complete")
- let parent = task.parentNode
-    parent.insertBefore(newdiv,task)
+    let task = document.getElementById("complete")
+    let parent = task.parentNode
+    parent.insertBefore(newdiv, task)
 }
 let el = document.getElementById("addTask");
 el.addEventListener("click", addTask, true);
@@ -19,15 +19,15 @@ el.addEventListener("click", addTask, true);
 
 function add_cTask() {
     let newdiv = document.createElement("div");
-    newdiv.innerHTML = "<input type='text' class='checks'>" +  "<img src='/images/checked.png' class='checked'>"
+    newdiv.innerHTML = "<input type='text' class='checks' placeholder='Description'>" + "<img src='/images/checked.png' class='checked'>" + "<input type='text' class='date' placeholder='deadline'"
     newdiv.className = "NewcTask"
- newdiv.setAttribute("draggable", "true");
- 
+    newdiv.setAttribute("draggable", "true");
+
     console.log("okay")
     document.getElementById("add_cTask").appendChild(newdiv)
     let ctask = document.getElementById("c-complete")
- let parents = ctask.parentNode
-    parents.insertBefore(newdiv,ctask)
+    let parents = ctask.parentNode
+    parents.insertBefore(newdiv, ctask)
 }
 
 function new_section() {
@@ -41,69 +41,69 @@ function new_section() {
 }
 
 function onDragStart(event) {
-  
+
     event
-      .dataTransfer
-      .setData('text/plain', event.target.id);
-      event
-      .currentTarget
-      ev.dataTransfer.dropEffect = "move";
-      evt.target.classList.add(`selected`);
-    
-  }
+        .dataTransfer
+        .setData('text/plain', event.target.id);
+    event
+        .currentTarget
+    ev.dataTransfer.dropEffect = "move";
+    evt.target.classList.add(`selected`);
+
+}
 
 
 
-  function DragEnd(event){
-event
-    .currentTarget
-    .document.getElementByClass(checked).src='/images/checked.png';
+function DragEnd(event) {
+    event
+        .currentTarget
+        .document.getElementByClass(checked).src = '/images/checked.png';
     evt.target.classList.remove(`selected`);
-    
-  }
 
-  function onDragOver(event) {
+}
+
+function onDragOver(event) {
     event.preventDefault();
     event
-    .currentTarget
-    .document.getElementById(checked).src='/images/checked.png';
+        .currentTarget
+        .document.getElementById(checked).src = '/images/checked.png';
     const activeElement = tasksListElement.querySelector(`.selected`);
     const currentElement = evt.target;
     const isMoveable = activeElement !== currentElement &&
-      currentElement.classList.contains(``);
-      
-    if (!isMoveable) {
-      return;
-    }
-    
-    const nextElement = (currentElement === activeElement.nextElementSibling) ?
-      currentElement.nextElementSibling :
-      currentElement;
-  
-    event.setAttribute("z-index","1000")
-    
-  
-  }
+        currentElement.classList.contains(``);
 
-  
-function myScript(){
-    
-    document.getElementById(checked).src='/images/checked.png';
+    if (!isMoveable) {
+        return;
+    }
+
+    const nextElement = (currentElement === activeElement.nextElementSibling) ?
+        currentElement.nextElementSibling :
+        currentElement;
+
+    event.setAttribute("z-index", "1000")
+
 
 }
-  function onDrop(event) {
+
+
+function myScript() {
+
+    document.getElementById(checked).src = '/images/checked.png';
+
+}
+
+function onDrop(event) {
     const id = event
-      .dataTransfer
-      .getData('text');
-      const draggableElement = document.getElementById(id);
-      const dropzone = event.target;
-      dropzone.appendChild(draggableElement);
-      event
-    .dataTransfer
-    .dataTransfer.dropEffect = "move"
-    .e.stopPropagation()
-    .clearDatа();
+        .dataTransfer
+        .getData('text');
+    const draggableElement = document.getElementById(id);
+    const dropzone = event.target;
+    dropzone.appendChild(draggableElement);
+    event
+        .dataTransfer
+        .dataTransfer.dropEffect = "move"
+        .e.stopPropagation()
+        .clearDatа();
 
     object.addEventListener("dragend", myScript);
-  }
-
+}
